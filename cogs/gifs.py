f0c815b9d80514027093dbe1d4c19ae1e4bb3302
Garvit Singh <garvitsingh4575@gmail.com>
2020-09-22 17:28:31 +0400

    GIFs 🦋🤦

--- cogs/gifs.py
+++ cogs/gifs.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from utils.functions import search_gif, get_gif_link
 from utils.lists import colors
 import random
-import time
 
 
 class GIFCommands(commands.Cog):
@@ -15,27 +14,20 @@ class GIFCommands(commands.Cog):
     async def kill(self, ctx, *, person):
 
         GIF = search_gif('kill anime', 30)
-        if 'beard' in person.clean_content.lower():
-            await ctx.send("Do you seriously think you can kill me? Idiot ")
-        else:
-            desc = ctx.author.name + ' kills ' + person
-            embed = discord.Embed(
-                title=None,
-                description=desc,
-                colour=random.choice(colors)
-            )
-            embed.set_image(url=get_gif_link(GIF))
-            await ctx.send(embed=embed)
+        desc = ctx.author.name + ' kills ' + person
+        embed = discord.Embed(
+            title=None,
+            description=desc,
+            colour=random.choice(colors)
+        )
+        embed.set_image(url=get_gif_link(GIF))
+        await ctx.send(embed=embed)
 
     @commands.command()
     @commands.cooldown(2, 30, commands.BucketType.user)
     async def punch(self, ctx, *, person):
         GIF = search_gif('punch anime', 30)
-        if 'beard' in person.clean_content.lower():
-            await ctx.send("You think you can punch me? Lol :joy:\nYou get punched now ")
-            desc = "RedBeard punches " + ctx.author.name
-        else:
-            desc = ctx.author.name + ' punches ' + person
+        desc = ctx.author.name + ' punches ' + person
         embed = discord.Embed(
             title=None,
             description=desc,
@@ -47,12 +39,8 @@ class GIFCommands(commands.Cog):
     @commands.command()
     @commands.cooldown(2, 30, commands.BucketType.user)
     async def slap(self, ctx, *, person):
-        GIF = search_gif('slap anime', 20)
-        if 'beard' in person.clean_content.lower():
-            await ctx.send("You think you can slap me? Lol :joy:\nYou get slapped now ")
-            desc = "RedBeard punches " + ctx.author.name
-        else:
-            desc = ctx.author.name + ' slaps ' + person
+        GIF = search_gif('slap anime', 30)
+        desc = ctx.author.name + ' slaps ' + person
         embed = discord.Embed(
             title=None,
             description=desc,
@@ -64,7 +52,7 @@ class GIFCommands(commands.Cog):
     @commands.command()
     @commands.cooldown(2, 30, commands.BucketType.user)
     async def stare(self, ctx, *, person):
-        GIF = search_gif('anime stare', 20)
+        GIF = search_gif('anime stare', 30)
         if person != "":
             desc = ctx.author.name + ' stares at ' + person
         else:
@@ -77,15 +65,10 @@ class GIFCommands(commands.Cog):
         embed.set_image(url=get_gif_link(GIF))
         await ctx.send(embed=embed)
 
-        if 'beard' in person.clean_content.lower():
-            async with ctx.typing():
-                time.sleep(1)
-            await ctx.send(":eyes:")
-
     @commands.command()
     @commands.cooldown(2, 30, commands.BucketType.user)
     async def hug(self, ctx, *, person):
-        GIF = search_gif('anime hug', 25)
+        GIF = search_gif('anime hug', 30)
         desc = ctx.author.name + ' hugs ' + person
         embed = discord.Embed(
             title=None,
@@ -95,20 +78,11 @@ class GIFCommands(commands.Cog):
         embed.set_image(url=get_gif_link(GIF))
         await ctx.send(embed=embed)
 
-        if 'beard' in person.clean_content.lower():
-            async with ctx.typing():
-                time.sleep(1)
-            await ctx.send("Alright I can give u a hug :hugging:")
-
     @commands.command()
     @commands.cooldown(2, 30, commands.BucketType.user)
     async def poke(self, ctx, *, person):
         GIF = search_gif('anime poke', 15)
-        if 'beard' in person.clean_content.lower():
-            await ctx.send("Nah nah nah, you dont poke me :) I poke you")
-            desc = "Redbeard pokes " + ctx.author.name
-        else:
-            desc = ctx.author.name + ' pokes ' + person
+        desc = ctx.author.name + ' pokes ' + person
         embed = discord.Embed(
             title=None,
             description=desc,
