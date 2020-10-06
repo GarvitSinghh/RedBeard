ce96e21e30aec985e84fcedf7cfd4166cc464db0
Garvit Singh <garvitsingh4575@gmail.com>
2020-10-06 16:03:07 +0400

    Adding AI Back 🤨

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 bot = commands.Bot(
