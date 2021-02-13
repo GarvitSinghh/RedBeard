8831faeb55a9695109d9d1cf25baac6f1901e80d
Garvit Singh <garvitsingh4575@gmail.com>
2021-02-13 15:31:41 +0400

    Setting up Server Cog 3 🙃

--- cogs/server.py
+++ cogs/server.py
@@ -11,31 +11,29 @@ class ServerCommands(commands.Cog, name='Server Commands'):
     @commands.command()
     @commands.guild_only()
     @commands.has_permissions(manage_channels=True)
-    async def setSuggestionChannel(ctx):
+    async def setSuggestionChannel(self, ctx):
         ctx.guild.suggestion_channel = ctx.channel
         await ctx.send("Current Channel has been set as the Suggestion Channel!")
 
     
     @commands.command()
     @commands.guild_only()
-    async def suggest(ctx, suggestion):
+    async def suggest(self, ctx, suggestion):
         try:
             channel = ctx.guild.suggestion_channel
-        except AttributeError:
-            await ctx.send("Please ask a moderator to setup a suggestion channel using setSuggestionChannel!")
-            return
-        
-        embed = discord.Embed(
+            embed = discord.Embed(
             title=suggestion,
             color=random.choice(colors)
-        )
-        embed.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.avatar_url)
-        message = await ctx.guild.suggestion_channel.send(embed=embed)
-        message.add_reaction("✅")
-        message.add_reaction("❌")
+            )
+            embed.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.avatar_url)
+            message = await ctx.guild.suggestion_channel.send(embed=embed)
+            message.add_reaction("✅")
+            message.add_reaction("❌")
+        
+        except AttributeError:
+            await ctx.send("Please ask a moderator to setup a suggestion channel using setSuggestionChannel!")
         
         await ctx.message.delete();
-
 
 def setup(bot):
     bot.add_cog(ServerCommands(bot))
