3c7e4f049f963ab9022819ae85b6498013bcfe93
Garvit Singh <garvitsingh4575@gmail.com>
2021-02-15 16:50:51 +0400

    Setting up Suggestion Command 🙂

--- cogs/server.py
+++ cogs/server.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from utils.lists import colors
 import random
 
+
 class ServerCommands(commands.Cog, name='Server Commands'):
 
     def __init__(self, bot):
@@ -16,27 +17,27 @@ class ServerCommands(commands.Cog, name='Server Commands'):
         self.bot.suggestion_channels[ctx.guild] = ctx.channel
         await ctx.send("Current Channel has been set as the Suggestion Channel!")
 
-    
     @commands.command()
     @commands.guild_only()
     async def suggest(self, ctx, suggestion):
 
-        await ctx.message.delete();
-        
+        await ctx.message.delete()
+
         channel = self.bot.get(ctx.guild, "not found")
         if channel != "not found":
             embed = discord.Embed(
-            title=suggestion,
-            color=random.choice(colors)
+                title=suggestion,
+                color=random.choice(colors)
             )
-            embed.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.avatar_url)
-            message = await ctx.guild.suggestion_channel.send(embed=embed)
+            embed.set_author(name=ctx.message.author.display_name,
+                             icon_url=ctx.message.author.avatar_url)
+            message = await channel.send(embed=embed)
             message.add_reaction("✅")
             message.add_reaction("❌")
-        
+
         else:
             await ctx.send("Please ask a moderator to set up a suggestion channel using `setSuggestionChannel`")
-        
+
 
 def setup(bot):
     bot.add_cog(ServerCommands(bot))
