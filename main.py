578c597dfd8bfb7ebc56a881ccbd3b66564dd280
Garvit Singh <garvitsingh4575@gmail.com>
2021-02-16 13:03:27 +0400

    Adding Intents 🥰

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get(
+    "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+
+intents = discord.Intents().all()
 
 bot = commands.Bot(
     command_prefix='',
-    case_insensitive=True
+    case_insensitive=True,
+    intents=intents
 )
 
 
@@ -51,7 +56,7 @@ async def ai(ctx):
         except Exception as error:
             await ctx.send("Hmmm")
             print(error)
-        
+
 
 for file in os.listdir('cogs'):
     if file.endswith(".py"):
